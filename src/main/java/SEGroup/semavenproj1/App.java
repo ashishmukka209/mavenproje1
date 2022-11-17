2cfca7e24db195a218da9dfcdb0c7b1029c5afb4
ashishmukka209 <ashishmukka209@gmail.com>
2022-11-17 11:09:32 +0530

    first commit

---
+++ src/main/java/SEGroup/semavenproj1/App.java
@@ -0,0 +1,12 @@
+package SEGroup.semavenproj1;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "This is my first Maven Java Project" );
+    }
+}
